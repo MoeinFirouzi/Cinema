e5a6a9be3e040cdce4af7ebea01551e223ac2c00
MoeinFirouzi <firouzi.m.moein@gmail.com>
2020-07-28 14:38:24 +0430

    Creat "Movie" modele

--- ticketing/models.py
+++ ticketing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    director = models.CharField(max_length=50)
+    year = models.IntegerField()
+    length = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
