93f563d6e02c58f533321402ee8a4ca96ce015c4
MoeinFirouzi <firouzi.m.moein@gmail.com>
2020-07-29 19:49:37 +0430

    Create "ShowTime" modele.Use "on_delete" and "choices in modele.This modele is connected to (relational) Movie ans Cinema classes

--- ticketing/models.py
+++ ticketing/models.py
@@ -23,3 +23,40 @@ class Cinema(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ShowTime(models.Model):
+    """
+    Represents a movie show in a cinema at specific time
+    Please read https://docs.djangoproject.com/en/3.0/ref/models/fields/#django.db.models.ForeignKey.on_delete
+    for more details about ForeignKey on_delete parameter
+
+    Also read:
+    https://docs.djangoproject.com/en/3.0/topics/db/models/
+    https://docs.djangoproject.com/en/3.0/topics/db/models/#relationships
+    """
+
+    movie = models.ForeignKey('Movie', on_delete=models.PROTECT)
+    cinema = models.ForeignKey('Cinema', on_delete=models.PROTECT)
+    start_time = models.DateTimeField()
+    price = models.IntegerField()
+    salable_seats = models.IntegerField()
+    free_seats = models.IntegerField()
+
+    SALE_NOT_STARTED = 1
+    SALE_OPEN = 2
+    TICKETS_SOLD = 3
+    SALE_CLOSED = 4
+    MOVIE_PLAYED = 5
+    SHOW_CANCELED = 6
+
+    status_choices = (
+        (SALE_NOT_STARTED, 'فروش آغاز نشده'),
+        (SALE_OPEN, 'در حال فروش بلیت'),
+        (TICKETS_SOLD, 'بلیت ها تمام شد'),
+        (SALE_CLOSED, 'فروش بلیت بسته شد'),
+        (MOVIE_PLAYED, 'فیلم پخش شد'),
+        (SHOW_CANCELED, 'سانس لغو شد')
+    )
+
+    status = models.IntegerField()
