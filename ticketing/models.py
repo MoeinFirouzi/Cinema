ef1f94ad33b670f70ac9eef6e600091c4d99cd97
MoeinFirouzi <firouzi.m.moein@gmail.com>
2020-07-29 13:19:05 +0430

    Create "Cinema" modele

--- ticketing/models.py
+++ ticketing/models.py
@@ -11,3 +11,15 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cinema(models.Model):
+    cinema_code = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    city = models.CharField(max_length=30, default='تهران')
+    capacity = models.IntegerField()
+    phone = models.CharField(max_length=20, null=True)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
