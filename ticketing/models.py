edfeb9c5eaac9ec83fe7dc603ba96b29220f3a59
MoeinFirouzi <firouzi.m.moein@gmail.com>
2020-07-30 23:26:53 +0430

    change Django admin tables to farsi

--- ticketing/models.py
+++ ticketing/models.py
@@ -3,23 +3,39 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    name = models.CharField(max_length=100)
-    director = models.CharField(max_length=50)
-    year = models.IntegerField()
-    length = models.IntegerField()
-    description = models.TextField()
+    class Meta:
+        """
+        Read :
+        https://docs.djangoproject.com/en/3.0/ref/models/options/
+        https://docs.djangoproject.com/en/3.0/ref/models/options/#verbose-name
+        """
+
+        verbose_name = 'فیلم'  # Normal form
+        verbose_name_plural = 'فیلم'  # plural form
+
+    name = models.CharField(max_length=100, verbose_name='نام فیلم')
+    # or
+    # name = models.CharField('نام فیلم', max_length=100)
+    director = models.CharField('تهیه کننده', max_length=50)
+    year = models.IntegerField('سال تولید')
+    length = models.IntegerField('مدت')
+    description = models.TextField('توضیحات')
 
     def __str__(self):
         return self.name
 
 
 class Cinema(models.Model):
-    cinema_code = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
-    city = models.CharField(max_length=30, default='تهران')
-    capacity = models.IntegerField()
-    phone = models.CharField(max_length=20, null=True)
-    address = models.TextField()
+    class Meta:
+        verbose_name = 'سینما'
+        verbose_name_plural = 'سینما'
+
+    cinema_code = models.IntegerField('کد سینما', primary_key=True)
+    name = models.CharField('نام سینما', max_length=50)
+    city = models.CharField('شهر', max_length=30, default='تهران')
+    capacity = models.IntegerField('ظرفیت')
+    phone = models.CharField('شماره تماس', max_length=20, null=True)
+    address = models.TextField('آدرس')
 
     def __str__(self):
         return self.name
@@ -36,12 +52,21 @@ class ShowTime(models.Model):
     https://docs.djangoproject.com/en/3.0/topics/db/models/#relationships
     """
 
-    movie = models.ForeignKey('Movie', on_delete=models.PROTECT)
-    cinema = models.ForeignKey('Cinema', on_delete=models.PROTECT)
-    start_time = models.DateTimeField()
-    price = models.IntegerField()
-    salable_seats = models.IntegerField()
-    free_seats = models.IntegerField()
+    class Meta:
+        verbose_name = 'سانس'
+        verbose_name_plural = 'سانس'
+
+    """
+    In Foreign key properties we should give related model in first parameter
+    so we can't give verbose name in first parameter and we should do like this: 
+    """
+    movie = models.ForeignKey('Movie', on_delete=models.PROTECT, verbose_name='فیلم')
+    cinema = models.ForeignKey('Cinema', on_delete=models.PROTECT, verbose_name='سینما')
+
+    start_time = models.DateTimeField('زمان شروع')
+    price = models.IntegerField('قیمت')
+    salable_seats = models.IntegerField('صندلی های قابل فروش')
+    free_seats = models.IntegerField('صندلی های خالی')
 
     SALE_NOT_STARTED = 1
     SALE_OPEN = 2
@@ -59,7 +84,7 @@ class ShowTime(models.Model):
         (SHOW_CANCELED, 'سانس لغو شد')
     )
 
-    status = models.IntegerField()
+    status = models.IntegerField('', choices=status_choices)
 
     def __str__(self):
         return '{} - {} - {}'.format(self.movie, self.cinema, self.start_time)
