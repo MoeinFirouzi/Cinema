1c3135f099d61f6e24aded21753ac922fa4221be
MoeinFirouzi <firouzi.m.moein@gmail.com>
2020-08-07 16:15:21 +0430

    Config urls and creat some simple view responses.

--- Cinema/urls.py
+++ Cinema/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ticketing/', include('ticketing.urls'))  # if include parameters be in 'app.urls' you don't need import app
 ]
--- ticketing/views.py
+++ ticketing/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from ticketing.models import Movie, Cinema
+
+
+def movie_list(request):
+    # 1- selecting data
+    movies = Movie.objects.all()
+    # 2- some code
+
+    # 3- render response
+    response_text = '\n'.join(
+        '{}: {}'.format(i, movie) for i, movie in enumerate(movies, start=1)
+    )
+    return HttpResponse(response_text)
+
+
+def cinema_list(request):
+    cinemas = Cinema.objects.all()
+    response_text = """
+    <html>
+    <head>
+    <title>لیست سینماها</title>
+    </head>
+    <body>
+        <h1>فهرست سینماهای کشور</h1>
+        <ul>
+            {}
+        </ul>
+    </body>
+    </html>
+    """.format(
+        '\n'.join('<li>{}</li>'.format(cinema) for cinema in cinemas)
+    )
+    return HttpResponse(response_text)
