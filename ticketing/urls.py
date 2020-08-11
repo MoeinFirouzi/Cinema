f2d7ed03a870a4c87cb50cd5d3e6096db4fad08c
MoeinFirouzi <firouzi.m.moein@gmail.com>
2020-08-12 01:31:05 +0430

    Show models details

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -7,4 +7,10 @@ app_name = 'ticketing'
 urlpatterns = [
     path('movie/list/', views.movie_list, name='movie_list'),
     path('cinema/list/', views.cinema_list, name='cinema_list'),
+    path('cinema/details/<int:cinema_id>',views.cinema_details, name='cinema_details'),
+    path('movie/details/<int:movie_id>', views.movie_details, name='movie_details')
+    # """
+    # read https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    # Also we can use RegEx
+    # """
 ]
--- ticketing/views.py
+++ ticketing/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from ticketing.models import Movie, Cinema
 
@@ -20,3 +21,30 @@ def cinema_list(request):
         'cinemas': cinemas
     }
     return render(request, 'ticketing/cinema_list.html', context)
+
+
+def movie_details(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    """
+        Use get() to return an object, or raise a Http404 exception if the object
+        does not exist.
+
+        klass may be a Model, Manager, or QuerySet object. All other passed
+        arguments and keyword arguments are used in the get() query.
+
+        Like with QuerySet.get(), MultipleObjectsReturned is raised if more than
+        one object is found.
+    """
+    context = {
+        'movie': movie
+    }
+    return render(request, 'ticketing/movie_details.html', context)
+
+
+def cinema_details(request, cinema_id):
+    cinema = get_object_or_404(Cinema, pk=cinema_id)
+    context = {
+        'cinema': cinema
+    }
+    return render(request, 'ticketing/cinema_details.html', context)
+    # return HttpResponse(cinema)
